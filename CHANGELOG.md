1c6aa76dabe180e83bacd7d68381eb51cfc765cc
Twan van Laarhoven <twanvl@gmail.com>
2018-09-03 21:43:58 +0200

    Use BaseMod.encounterList to get a list of monsters

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+### 0.2.2
+* Use BaseMod.encounterList to also export monsters from custom encounters.
+
+### 0.2.1
+* Compatibility with ModTheSpire and BaseMod v3.0.0
+
--- src/main/java/sts_exporter/CreatureExportData.java
+++ src/main/java/sts_exporter/CreatureExportData.java
@@ -25,6 +25,8 @@ import com.megacrit.cardcrawl.monsters.MonsterGroup;
 import com.megacrit.cardcrawl.monsters.exordium.Lagavulin;
 import com.megacrit.cardcrawl.rooms.EmptyRoom;
 
+import basemod.BaseMod;
+
 public class CreatureExportData {
     public AbstractCreature creature;
     public String image, relImage;
@@ -120,140 +122,14 @@ public class CreatureExportData {
         // Awake lagavulin looks different
         Lagavulin lagavulin = new Lagavulin(false);
         lagavulin.id = lagavulin.id + "Awake";
+        lagavulin.name = lagavulin.name + " (Awake)";
         creatures.add(lagavulin);
 
         return creatures;
     }
 
-    public static ArrayList<String> getAllMonsterNames() {
-        // these don't appear in encounters
-        ArrayList<String> out = new ArrayList<>();
-        out.add("BronzeOrb");
-        out.add("TorchHead");
-        return out;
-    }
-
     public static Collection<String> getAllEncounterNames() {
-        // copied from MonsterHelper.uploadEnemyData
-        ArrayList<EnemyData> data = new ArrayList<EnemyData>();
-        data.add(new EnemyData("Blue Slaver", 1, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Cultist", 1, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Jaw Worm", 1, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("2 Louse", 1, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Small Slimes", 1, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Gremlin Gang", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Large Slime", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Looter", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Lots of Slimes", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Exordium Thugs", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Exordium Wildlife", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Red Slaver", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("3 Louse", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("2 Fungi Beasts", 1, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Gremlin Nob", 1, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("Lagavulin", 1, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("3 Sentries", 1, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("Lagavulin Event", 1, EnemyData.MonsterType.EVENT));
-        data.add(new EnemyData("Mushrooms", 1, EnemyData.MonsterType.EVENT));
-        data.add(new EnemyData("The Guardian", 1, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Hexaghost", 1, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Slime Boss", 1, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Chosen", 2, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Shell Parasite", 2, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Spheric Guardian", 2, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("3 Byrds", 2, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("2 Thieves", 2, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Chosen and Byrds", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Sentry and Sphere", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Slaver and Parasite", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Snake Plant", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Snecko", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Centurion and Healer", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Cultist and Chosen", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("3 Cultists", 2, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Gremlin Leader", 2, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("Slavers", 2, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("Book of Stabbing", 2, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("Masked Bandits", 2, EnemyData.MonsterType.EVENT));
-        data.add(new EnemyData("Automaton", 2, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Champ", 2, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Collector", 2, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Flame Bruiser 1 Orb", 3, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Orb Walker", 3, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("3 Darklings", 3, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("3 Shapes", 3, EnemyData.MonsterType.WEAK));
-        data.add(new EnemyData("Flame Bruiser 2 Orb", 3, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("4 Shapes", 3, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Maw", 3, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Puppeteer", 3, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Sphere and 2 Shapes", 3, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Spire Growth", 3, EnemyData.MonsterType.STRONG));
-        data.add(new EnemyData("Giant Head", 3, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("Nemesis", 3, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("2 Orb Walkers", 3, EnemyData.MonsterType.ELITE));
-        data.add(new EnemyData("Mysterious Sphere", 3, EnemyData.MonsterType.EVENT));
-        data.add(new EnemyData("Awakened One", 3, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Donu and Deca", 3, EnemyData.MonsterType.BOSS));
-        data.add(new EnemyData("Time Eater", 3, EnemyData.MonsterType.BOSS));
-        ArrayList<String> encounters = new ArrayList<String>();
-        for (EnemyData d : data) {
-            encounters.add(d.name);
-        }
-        return encounters;
+        return BaseMod.encounterList;
     }
 
-    public static Collection<String> getAllEncounterNamesFromDungeons() {
-        // Hacky way to get all monster names
-        // This unfortunately breaks the game somewhat
-        HashSet<String> encounterNames = new HashSet<>();
-        AbstractPlayer p = new Ironclad("Exporter", AbstractPlayer.PlayerClass.IRONCLAD);
-        ArrayList<String> emptyList = new ArrayList<String>();
-        getAllEncounterNames(encounterNames, new Exordium(p, emptyList));
-        getAllEncounterNames(encounterNames, new TheCity(p, emptyList));
-        getAllEncounterNames(encounterNames, new TheBeyond(p, emptyList));
-        // Better to hard-code encounter lists
-        encounterNames.add("Spheric Guardian");
-        encounterNames.add("Chosen");
-        encounterNames.add("Shell Parasite");
-        encounterNames.add("3 Byrds");
-        encounterNames.add("2 Thieves");
-        encounterNames.add("Chosen and Byrds");
-        encounterNames.add("Sentry and Sphere");
-        encounterNames.add("Slaver and Parasite");
-        encounterNames.add("Snake Plant");
-        encounterNames.add("Snecko");
-        encounterNames.add("Centurion and Healer");
-        encounterNames.add("Cultist and Chosen");
-        encounterNames.add("3 Cultists");
-        encounterNames.add("Gremlin Leader");
-        encounterNames.add("Slavers");
-        encounterNames.add("Book of Stabbing");
-        // bosses might need unlocks
-        encounterNames.add("The Guardian");
-        encounterNames.add("Hexaghost");
-        encounterNames.add("Slime Boss");
-        encounterNames.add("Awakened One");
-        encounterNames.add("Time Eater");
-        encounterNames.add("Donu and Deca");
-        encounterNames.add("Automaton");
-        encounterNames.add("Collector");
-        encounterNames.add("Champ");
-        // clean up a bit
-        return encounterNames;
-    }
-    private static void getAllEncounterNames(HashSet<String> encounterNames, AbstractDungeon dungeon) {
-        // there is some randomness in the encounter generation, so just do a couple of loops to get them all
-        for (int i = 0 ; i < 10 ; i++) {
-            try {
-                Method method = AbstractDungeon.class.getDeclaredMethod("generateMonsters");
-                method.setAccessible(true);
-                method.invoke(dungeon);
-            } catch (Exception e) {
-                Exporter.logger.error("Exception occured when calling generateMonsters", e);
-            }
-            for (String e : AbstractDungeon.bossList) encounterNames.add(e);
-            for (String e : AbstractDungeon.monsterList) encounterNames.add(e);
-            for (String e : AbstractDungeon.eliteMonsterList) encounterNames.add(e);
-        }
-    }
 }
